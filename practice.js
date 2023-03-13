7801484c16bfc875ab12b3f95dd9fbbe927ab3cc
Colleen Stewart <collstewart30@gmail.com>
2023-03-13 14:29:56 -0400

    variables, data types, operators video examples

---
+++ practice.js
@@ -0,0 +1,35 @@
+// programmig - management and movement of data
+
+console.log("welcome to JS foundations");
+
+
+// VARIABLES, DATA TYPES, AND OPERATORS
+// - variables: used to store and manipulate data
+// - data types: inform what you can and cannot do with a variable
+// - operators: allows us to manipulate the values of variables, assign a new value to a variable, compare values of variables
+
+
+let numberOne = 2;
+let numberTwo = 3;
+
+let sum = numberOne + numberTwo;
+console.log(sum);
+ 
+let firstName = "Colleen";
+let lastName = "Stewart";
+
+let fullName = firstName + " " + lastName;
+console.log(fullName)
+
+// Comparison operators
+
+let myName = "Colleen";
+let yourName = "Logan";
+let areEqualNames = myName === yourName;
+console.log(areEqualNames)
+
+// Logical operators
+
+let age = 16;
+let isTeenager = age >= 13 && age <= 19;
+console.log(isTeenager)
