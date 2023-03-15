0b219d1a63ca369933a63aba48400533e5abe883
Colleen Stewart <collstewart30@gmail.com>
2023-03-15 15:48:24 -0400

    Loops video

---
+++ assignment.js
@@ -0,0 +1,22 @@
+// Variables, Data Types, and Operators Task: Pay Calculator v1
+
+// Create a "pay calculator" to determine how much money someone has made. 
+// Overtime is NOT taken into account.
+// 1) Declare a variable called 'hoursWorked' and assign it the value of 35.
+// 2) Declare a second variable called 'payRate' and assign it the value of 7.25. 
+// 3) Next, declare a variable called 'totalPay', and use an operator to give it the value of the hoursWorked multiplied by the payRate.
+// 4) Lastly, display 'totalPay' in the console. The output should be 253.75!
+
+
+// let hoursWorked = 45;
+// let payRate = 7.25;
+// let totalPay = hoursWorked * payRate;
+// let overTimePayRate = payRate * 1.5;
+
+// if(hoursWorked > 40){
+//     totalPay = ((hoursWorked - 40) * overTimePayRate) + (40 * payRate);
+//     console.log(totalPay);
+// }
+// else{
+//     console.log(totalPay);    
+// }
--- practice.js
+++ practice.js
@@ -97,19 +97,54 @@ else{
 
 // switch/case
 
-let userInput = prompt("Enter a number between 1 and 4")
+// let userInput = prompt("Enter a number between 1 and 4")
 
-switch(userInput){
-    case "1":
-        console.log("Attacking!");
+// switch(userInput){
+//     case "1":
+//         console.log("Attacking!");
+//         break;
+//     case "2":
+//         console.log("Defending!");
+//         break;
+//     case "3":
+//         console.log("Evading!");
+//         break;
+//     default:
+//         console.log("Whoops, try again!");
+//         break;
+// }
+
+
+
+let revenue = 1000;
+if(revenue >= 500){
+    console.log("Made a profit!");
+}
+else if(revenue > 0 && revenue <= 499){
+    console.log("At least we are still above 0!")
+}
+else{
+    console.log("We made a loss!")
+}
+
+
+let bestFootballTeam = 'Eagles';
+
+switch(bestFootballTeam){
+    case "Eagles":
+        console.log("best team in the world");
         break;
-    case "2":
-        console.log("Defending!");
+    case "Bears":
+        console.log("boo");
         break;
-    case "3":
-        console.log("Evading!");
+    case "Packers":
+        console.log("they will never win a Super Bowl");
         break;
     default:
-        console.log("Whoops, try again!");
+        console.log("I guess you don't know much about football");
         break;
+}
+
+for(let i = 0; i < 10; i ++){
+    console.log(i)
 }
